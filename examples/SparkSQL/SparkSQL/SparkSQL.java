31119ef576fbd0a027ed08e9a6674fcc7da53524
s1401k008 <cathy90703@gmail.com>
2016-08-25 23:51:54 +0800

    Fix SparkSQL.java example ...

--- examples/SparkSQL/SparkSQL/SparkSQL.java
+++ examples/SparkSQL/SparkSQL/SparkSQL.java
@@ -6,13 +6,29 @@ import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
 
 public class SparkSQL {
+
 	public static void main(String[] args) {
-	    SparkConf sc = new SparkConf().setAppName("SparkSQL");
+		//spark 基本設定
+		SparkConf sc = new SparkConf().setAppName("SparkSQL");
 	    JavaSparkContext ctx = new JavaSparkContext(sc);
 	    SQLContext sqlContext = new SQLContext(ctx);
 	    
-	    DataFrame people = sqlContext.read().json(args[0]);
-	    people.select("juice").show();
+	    //將外部 JSON 轉換成 DateFrame 資料表
+	    DataFrame drinks = sqlContext.read().json(args[0]);
+	    
+	    drinks.printSchema();
+	    //查看資料表內容
+	    drinks.show();
+	    
+	    //用關聯式語法查詢
+	    drinks.registerTempTable("drinks");
+	    DataFrame choices = sqlContext.sql("SELECT juice FROM drinks");
+	    choices.show();
+	    
+	    //用SparkSQL API 作關聯式語法查詢
+	    drinks.select("juice").show();
+	    
+	    
 	}
 
 }
